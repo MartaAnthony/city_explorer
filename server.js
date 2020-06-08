cfe5b2b2e410ef24b34f6ad8f43aedcdb7cb1ec2
Marta <marta.anthony1@gmail.com>
2020-06-08 15:39:18 -0700

    x

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (request, response) => {
-  console.log('hello out there');
-  response.status(200).send('I like pizza');
+  console.log('oh hello');
+  response.status(200).send('I like WA');
 });
 
 app.get('/location', (request, response) => {
@@ -25,46 +25,29 @@ app.get('/location', (request, response) => {
   let returnObj = new Location(search_query, geoData[0]);
 
   console.log(returnObj);
-  // let returnObj = {
-  //   search_query: search_query,
-  //   formatted_query: geoData[0].display_name,
-  //   latitude: geoData[0].lat,
-  //   longitude: geoData[0].lon
-  // }
 
   response.status(200).send(returnObj);
-
-  // // app.get('/bananas', (request, response) => {
-  // //   console.log('it is Monday');
-  // //   response.status(200).send('tell me about it');
-  // // });
-
-  // // app.get('/pizza', (request, response) => {
-  // //   response.status(200).send('I am on the pizza route');
-  // // });
-
-  //
 
   //   } catch(err){
   //     console.log('ERROR', err);
   //     response.status(500).send('sorry, we messed up');
   //   }
 
-  // })
-
-  function Location(searchQuery, obj){
-    this.search_query = searchQuery;
-    this.formatted_query = obj.display_name;
-    this.latitude = obj.lat;
-    this.longitude = obj.lon;
-  }
-
-  // app.get('*', (request, response) => {
-  //   response.status(404).send('sorry, this route does not exist');
-  // })
-
-  // // turn on the lights - move into the house - start the server
-  app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
-  });
 })
+
+function Location(searchQuery, obj){
+  this.search_query = searchQuery;
+  this.formatted_query = obj.display_name;
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
+}
+
+// app.get('*', (request, response) => {
+//   response.status(404).send('sorry, this route does not exist');
+// })
+
+// // turn on the lights - move into the house - start the server
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
+
