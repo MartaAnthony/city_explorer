6fcadd1cf1aaed69d9a7bfe8336a872446f917c1
Marta <marta.anthony1@gmail.com>
2020-06-08 14:04:00 -0700

    repo setup

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # city_explorer
+
+**Author**: Marta Anthony
+**Version**: 1.0.0 
+
+## Overview
+The application is a stand-alone back end which interacts with a static front end. It requests data from a total of six third-party APIs, modifies the data, and sends the data to the client to be displayed in the browser. It provides data for the City Explorer Application, allowing a user to search for a location, present a Map, as well as interesting information about the area, all using data from APIs that the server will fetch and manage.
+
+## Getting Started
+Set up the repository.
+
+## Architecture
+JavaScript
+Node.js
+Express
+
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+
+## Credits and Collaborations
+
+
+Number and name of feature: ________________________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
---
+++ server.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+const cors = require('cors');
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+// // app.get('/', (request, response) => {
+// //   console.log('hello out there');
+// //   response.status(200).send('I like pizza');
+// // });
+
+// // app.get('/bananas', (request, response) => {
+// //   console.log('it is Monday');
+// //   response.status(200).send('tell me about it');
+// // });
+
+// // app.get('/pizza', (request, response) => {
+// //   response.status(200).send('I am on the pizza route');
+// // });
+
+// app.get('/location', (request, response) => {
+//   try{
+//     // query: { city: 'seattle' },
+//     console.log(request.query.city);
+//     let search_query = request.query.city;
+  
+//     let geoData = require('./data/location.json');
+  
+//     let returnObj = new Location(search_query, geoData[0]);
+  
+//     console.log(returnObj);
+//     // let returnObj = {
+//     //   search_query: search_query,
+//     //   formatted_query: geoData[0].display_name,
+//     //   latitude: geoData[0].lat,
+//     //   longitude: geoData[0].lon
+//     // }
+  
+//     response.status(200).send(returnObj);
+    
+//   } catch(err){
+//     console.log('ERROR', err);
+//     response.status(500).send('sorry, we messed up');
+//   }
+
+// })
+
+// function Location(searchQuery, obj){
+//   this.search_query = searchQuery;
+//   this.formatted_query = obj.display_name;
+//   this.latitude = obj.lat;
+//   this.longitude = obj.lon;
+// }
+
+// app.get('*', (request, response) => {
+//   response.status(404).send('sorry, this route does not exist');
+// })
+
+// // turn on the lights - move into the house - start the server
+// app.listen(PORT, () => {
+//   console.log(`listening on ${PORT}`);
+// })
